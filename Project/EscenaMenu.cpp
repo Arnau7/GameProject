790a0834003f4c8574d9170f9e42ff4fc8368597
Arnau7 <arnauruiz1b@gmail.com>
2016-12-30 16:39:31 +0100

    Escena Menu

---
+++ Project/EscenaMenu.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int dif;
+	cout << "Please select your game dificulty:\n\n";
+	cout << "	Press < 1 > for EASY mode\n\n";
+	cout << "	Press < 2 > for NORMAL mode\n\n";
+	cout << "	Press < 3 > for HARD mode\n\n";
+	cin >> dif;
+	
+	switch (dif) {
+	case 1:
+		cout << "EASY mode selected\n";
+		break;
+	case 2:
+		cout << "NORMAL mode selected\n";
+		break;
+	case 3:
+		cout << "HARD mode selected\n";
+		break;
+	default:
+		cout << "Error. Press 1, 2 or 3 to select a game mode.\n";
+		break;
+	}
+
+	return 0;
+}
